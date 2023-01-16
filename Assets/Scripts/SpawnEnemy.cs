eb01218513d621aedc5aaa1f7ea5d5188288f7c0
Vetolisk <volgany@gmail.com>
2023-01-16 12:42:10 +0300

    FixSpawn

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -6,14 +6,17 @@ public class SpawnEnemy : MonoBehaviour
 {
     public GameObject preabEnemy;
     public int Count;
-    private int length;
+   
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < Count; i++)
         {
-            GameObject CloneEnemy = Instantiate(preabEnemy, new Vector3(gameObject.transform.position.x+length, gameObject.transform.position.y, gameObject.transform.position.z), Quaternion.identity) as GameObject;
-            length -= 4;
+            for (int j = 0; i < Count; i++)
+            {
+                GameObject CloneEnemy = Instantiate(preabEnemy, new Vector3(gameObject.transform.position.x + i, gameObject.transform.position.y, gameObject.transform.position.z+j), Quaternion.identity) as GameObject;
+            }
+            
         }
        
     }
